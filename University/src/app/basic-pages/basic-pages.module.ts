60bc68f432e5a7cefeaf4804d014c399f5703a73
sari avramovich <s0556766361@gmail.com>
2024-03-14 00:40:47 +0200

    13

--- University/src/app/basic-pages/basic-pages.module.ts
+++ University/src/app/basic-pages/basic-pages.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { UserService } from '../user/user.service';
 
 
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, HeaderComponent, HomeComponent],
+  providers: [UserService]
 })
 export class BasicPagesModule { }
